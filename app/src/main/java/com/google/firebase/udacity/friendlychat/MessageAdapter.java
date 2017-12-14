7179d44e5618fdd6e8bad69fc083f891644f2d73
liye0717 <liubou.yerameyeva@netcracker.com>
2017-12-14 18:09:23 +0300

    - moved constants and separate classes - anonymous classes replaced with lambdas (before adding the RecyclerView)

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MessageAdapter.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MessageAdapter.java
@@ -2,6 +2,7 @@ package com.google.firebase.udacity.friendlychat;
 
 import android.app.Activity;
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -9,6 +10,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.udacity.friendlychat.model.FriendlyMessage;
 
 import java.util.List;
 
@@ -17,8 +19,9 @@ public class MessageAdapter extends ArrayAdapter<FriendlyMessage> {
         super(context, resource, objects);
     }
 
+    @NonNull
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
         if (convertView == null) {
             convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.item_message, parent, false);
         }
---
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/constant/FriendlyChatConstants.java
@@ -0,0 +1,12 @@
+package com.google.firebase.udacity.friendlychat.constant;
+
+public final class FriendlyChatConstants {
+    public static final String TAG = "MainActivity";
+
+    public static final String ANONYMOUS = "anonymous";
+    public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
+    public static final String FRIENDLY_MSG_LENGTH_KEY = "friendly_msg_length";
+
+    public static final int RC_SIGN_IN = 1;
+    public static final int RC_PHOTO_PICKER = 2;
+}
