20cf94147e6ff4e412cafb93056bb4ee3e8388d6
liye0717 <liubou.yerameyeva@netcracker.com>
2017-12-16 22:37:48 +0300

    + onError in Recycler Adaptor

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -27,6 +27,7 @@ import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
@@ -112,6 +113,12 @@ public class MainActivity extends AppCompatActivity {
             public void onDataChanged() {
                 super.onDataChanged();
                 messagesRecyclerView.scrollToPosition(getItemCount() - 1);
+            }
+
+            @Override
+            public void onError(DatabaseError error) {
+                super.onError(error);
+                Toast.makeText(MainActivity.this, "Error while getting data", Toast.LENGTH_SHORT).show();
             }
         };
 
